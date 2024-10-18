7d005cacc63bf2af0e73584d2024c37adf7e944b
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2024-10-18 11:54:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Set all the bits in given range of a number/set-all-the-bits-in-given-range-of-a-number.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int setAllRangeBits(int N , int l , int r) {
+        // code here
+        for(int i = l - 1; i < r; i++) {
+            N = N | (1 << i);
+        }
+        return N;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,L,R;
+        
+        cin>>N>>L>>R;
+
+        Solution ob;
+        cout << ob.setAllRangeBits(N,L,R) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
