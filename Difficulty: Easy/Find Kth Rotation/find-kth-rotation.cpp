fdb838f51015570add61a171d54b8bfcc6aac401
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2026-02-09 23:30:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Kth Rotation/find-kth-rotation.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int findKRotation(vector<int> &arr) {
+        // Code Here
+        int n = arr.size();
+      int count = 0;
+      int flag = 0;
+      for(int i = n-1; i>=1; i--){
+          if(arr[i]<arr[i-1]||flag == 1){
+              flag = 1;
+              count++;
+          }
+      }
+      return count;
+      
+    }
+};
